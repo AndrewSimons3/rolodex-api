6049c8de25ffe6f9ffa624fa295d8da16a1b42d7
AndrewSimons3 <simdrew07@gmail.com>
2020-03-25 21:44:16 -0500

    added users and thumbnail

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+class App extends React.Component {
+  state = {
+    users: []
+  }
+  componentDidMount() {
+    fetch("https://randomuser.me/api?results=25").then( (resp) => {
+      return resp.json();
+  }).then( (data) => {
+    this.setState({users: data.results})
+  });
+  }
+
+  getUserName (user) {
+    return `${user.name.title} ${user.name.first} ${user.name.last}`
+  }
+
+  isFlagged (user) {
+    let newDetails = this.state.users.map((aUser) => {
+      if(aUser.id === user.id) {
+        aUser.isFlagged = !aUser.isFlagged
+    return `${user.location} ${user.email} ${user.login} ${user.dob}`
+      
+  }
+
+
+render() {
+  // let isLoading = !this.state.users || this.state.users.length === 0
+  // if (isLoading) {
+  //   return ((<div>loading...</div>))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <h1>
+        User List 
+      </h1>
+      <ul>
+      {this.state.users.map((user, index) => {
+        return (<li key={index}>
+          <span>{this.getUserName(user)}</span>
+          <img src={user.picture.thumbnail} alt='user thumbnail img'></img>
+          <button onClick={this.isFlagged.bind(this, user)}>{user.isFlagged ? "Hide Details" : "Show Details"}</button>
+        </li>)
+      })}
+      </ul>
     </div>
   );
-}
+}}
 
 export default App;
